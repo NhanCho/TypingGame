2749b1a8acae4cf73d7ee60283eabc73f47a713f
NhanCho <itsmetremor1123@gmail.com>
2023-11-20 17:14:30 +0700

    them hieu ung keyboard va thong bao het thoi gian

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ let Words = [
 let random = Math.floor(Math.random() * Words.length);
 
 let timerHTML = document.getElementById("time_Set");
-let timerStop = document.getElementById("btn_Stop");
+// let timerStop = document.getElementById("btn_Stop");
+let noti = document.getElementById("notification");
 let timerRestart = document.getElementById("btn_Restart");
 let placeWords = document.getElementById("place_Word");
 let scoreBoard = document.getElementById("number_Score");
@@ -42,15 +43,24 @@ function Timer() {
   if (timerHTML.innerHTML <= 0) {
     //het tg thi reset
     // alert("Het thoi gian");
-    timeReset();
+    timeStop();
   } else {
     //Chưa hết thì vẫn đếm ngược
     timerHTML.innerHTML = parseInt(timerHTML.innerHTML) - 1;
   }
 }
 
+function timeStop() {
+  clearInterval(Timer)
+  timerHTML.innerHTML = 0;
+  noti.innerHTML = "HET THOI GIAN!"
+  document.getElementById('input_Word').readOnly = true;
+}
+
 function timeReset() {
   Score = 0;
+  noti.innerHTML = ""
+  document.getElementById('input_Word').readOnly = false;
   addScore();
   timerHTML.innerHTML = 10;
 }
@@ -69,6 +79,30 @@ Input.addEventListener("keyup", (event) => {
       addScore();
     }
   }
+});
+
+function handleKeyDown(key) {
+  //Tao tempKey voi key duoc bien doi thanh chu viet hoa
+  const tempKey = key.toUpperCase();
+  //META la nut window
+  if (tempKey === " " || tempKey === "CONTROL" || tempKey === "ALT" || tempKey === "META") {
+    return; //Ne cac truong hop bam nut dac biet
+  }
+  //lay trang thai cua tempKey (da duoc in hoa)
+  let keyElement = document.getElementById(tempKey);
+  console.log(keyElement);
+  //add class Click
+  keyElement.classList.add("Click");
+  //bo class Click
+  const handleKeyUp = () => {
+
+    keyElement.classList.remove("Click");
+    window.removeEventListener("keyup", handleKeyUp);
+  };
+  window.addEventListener("keyup", handleKeyUp);
+}
+window.addEventListener("keydown", ({ key }) => {
+  handleKeyDown(key);
 });
 // console.log(timerInterval)
 // console.log(Score);
